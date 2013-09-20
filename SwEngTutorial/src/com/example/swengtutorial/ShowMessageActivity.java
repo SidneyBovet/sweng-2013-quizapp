09a61c8c857950a1872225cc26157aad11377468
Joanna Salathe <jsalathe@joanna-linux.(none)>
2013-09-20 15:50:57 +0200

    Initial import

---
+++ SwEngTutorial/src/com/example/swengtutorial/ShowMessageActivity.java
@@ -0,0 +1,35 @@
+package com.example.swengtutorial;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class ShowMessageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_show_message);
+
+	    // get Intent that started this Activity
+	    Intent startingIntent = getIntent();
+
+	    // get the value of the user string
+	    String userText = startingIntent.getStringExtra(MainActivity.class.getName());
+
+	    // get the TextView on which we are going to show the string, and update
+	    // its contents
+	    TextView textView = (TextView) findViewById(R.id.displayed_text);
+	    textView.setText(userText);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.show_message, menu);
+		return true;
+	}
+
+}
