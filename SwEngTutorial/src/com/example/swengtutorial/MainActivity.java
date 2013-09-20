525ac05481388dbf2ef812f1af04250fb1cfd69a
MelodyLucid <melo.lucid@gmail.com>
2013-09-20 16:56:44 +0200

    Conflict resolve

--- SwEngTutorial/src/com/example/swengtutorial/MainActivity.java
+++ SwEngTutorial/src/com/example/swengtutorial/MainActivity.java
@@ -1,18 +1,17 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> f045a7d1588d1bba44162d724618e77cd32675b6
 package com.example.swengtutorial;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-<<<<<<< HEAD
-
+/***
+ * 
+ * @author Merok
+ *
+ */
 public class MainActivity extends Activity {
 
     @Override
@@ -30,45 +29,13 @@ public class MainActivity extends Activity {
     }
     
     public void displayMessage(View view) {
-        Toast.makeText(this, "You clicked Display!", Toast.LENGTH_SHORT).show();
-        Intent displayActivityIntent = new Intent(this, ShowMessageActivity.class);
+    	Toast.makeText(this, "You clicked Display!", Toast.LENGTH_SHORT).show();
+    	Intent displayActivityIntent = new Intent(this, ShowMessageActivity.class);
+    	
         EditText editText = (EditText) findViewById(R.id.edit_text);
         String userText = editText.getText().toString();
+        
         displayActivityIntent.putExtra(this.getClass().getName(), userText);
-        startActivity(displayActivityIntent);
+    	startActivity(displayActivityIntent);
     }
-=======
-/***
- * 
- * @author Merok
- *
- */
-public class MainActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	public void displayMessage(View view) {
-		Toast.makeText(this, "You clicked Display", Toast.LENGTH_SHORT).show();
-		Intent displayActivityIntent = new Intent(this,
-				ShowMessageActivity.class);
-
-		EditText editText = (EditText) findViewById(R.id.edit_text);
-		String userText = editText.getText().toString();
-		displayActivityIntent.putExtra(this.getClass().getName(), userText);
-
-		startActivity(displayActivityIntent);
-
-	}
->>>>>>> f045a7d1588d1bba44162d724618e77cd32675b6
 }
--- SwEngTutorial/src/com/example/swengtutorial/ShowMessageActivity.java
+++ SwEngTutorial/src/com/example/swengtutorial/ShowMessageActivity.java
@@ -5,15 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.widget.TextView;
-<<<<<<< HEAD
 
-=======
-/***
- * 
- * @author Merok
- *
- */
->>>>>>> f045a7d1588d1bba44162d724618e77cd32675b6
 public class ShowMessageActivity extends Activity {
 
 	@Override
@@ -21,27 +13,16 @@ public class ShowMessageActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_show_message);
 
-<<<<<<< HEAD
-	    // get Intent that started this Activity
-	    Intent startingIntent = getIntent();
-
-	    // get the value of the user string
-	    String userText = startingIntent.getStringExtra(MainActivity.class.getName());
-
-	    // get the TextView on which we are going to show the string, and update
-	    // its contents
-	    TextView textView = (TextView) findViewById(R.id.displayed_text);
-	    textView.setText(userText);
-=======
-		// get the Intent that started this Activity
+		// get Intent that started this Activity
 		Intent startingIntent = getIntent();
-		
+
 		// get the value of the user string
-		String userString = startingIntent.getStringExtra(MainActivity.class.getName());
-		
-		TextView textview = (TextView) findViewById(R.id.displayed_text);
-		textview.setText(userString);
->>>>>>> f045a7d1588d1bba44162d724618e77cd32675b6
+		String userText = startingIntent.getStringExtra(MainActivity.class.getName());
+
+		// get the TextView on which we are going to show the string, and update
+		// its contents
+		TextView textView = (TextView) findViewById(R.id.displayed_text);
+		textView.setText(userText);
 	}
 
 	@Override
