35590242b7e04bca0d7d3eb47687d923a0ea51ad
David Dieulivol <david.dieulivol@epfl.ch>
2013-09-24 11:29:00 +0200

    Section 3.1 : Creation of the basic activities (NEEDS TO BE TESTED)

---
+++ SwEng2013QuizApp/src/epfl/sweng/DisplayQuestionActivity.java
@@ -0,0 +1,22 @@
+package epfl.sweng;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class DisplayQuestionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_display_question);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.display_question, menu);
+		return true;
+	}
+
+}
---
+++ SwEng2013QuizApp/src/epfl/sweng/entry/SubmitQuestionActivity.java
@@ -0,0 +1,23 @@
+package epfl.sweng.entry;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import epfl.sweng.R;
+
+public class SubmitQuestionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_submit_question);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.submit_question, menu);
+		return true;
+	}
+
+}
