03ca5344b11517e78aff45db426b4d96437fd52d
Joanna Salathe <joanna.salathe@epfl.ch>
2013-09-24 18:44:09 +0200

    Conflict resolve on Question.java

--- SwEng2013QuizApp/src/epfl/sweng/backend/Question.java
+++ SwEng2013QuizApp/src/epfl/sweng/backend/Question.java
@@ -10,11 +10,6 @@ import org.json.JSONObject;
  * 
  */
 public class Question {
-<<<<<<< HEAD
-	
-=======
-
->>>>>>> b617fb33fcd79d60a45f67126175b2dc04287de4
 	private long id;
 	private String questionContent;
 	private String[] answers;
@@ -24,24 +19,6 @@ public class Question {
 
 	/**
 	 * 
-<<<<<<< HEAD
-	 * @param id 				: long that uniquely identifies a question.
-	 * @param question 			: String that contains the text of the question.
-	 * @param answers			: List of String objects that contains the possible answers. 
-	 * 							  The list is 0-indexed, meaning that the first element�s index is 0.
-	 * 							  The order of the answers is important, because the correct answer is 
-	 * 							  identified by its index in the answers list.
-	 * @param solutionIndex		: Int that indicates the correct answer by its position in the answers list.
-	 * @param tags				: Set of question tags.
-	 * @param owner				: Question owner.
-	 */
-	public Question(long id, 
-					String question, 
-					String[] answers, 
-					int solutionIndex, 
-					String[] tags, 
-					String owner) {
-=======
 	 * @param id
 	 *            : long that uniquely identifies a question.
 	 * @param question
@@ -61,7 +38,6 @@ public class Question {
 	 */
 	public Question(long id, String question, String[] answers,
 			int solutionIndex, String[] tags, String owner) {
->>>>>>> b617fb33fcd79d60a45f67126175b2dc04287de4
 		this.id = id;
 		this.questionContent = question;
 		this.answers = answers;
@@ -74,10 +50,6 @@ public class Question {
 			throws JSONException {
 
 		JSONObject jsonParser = new JSONObject(questionJSON);
-<<<<<<< HEAD
-=======
-		
->>>>>>> b617fb33fcd79d60a45f67126175b2dc04287de4
 		long id = Long.parseLong((String) jsonParser.get("id"));
 		String question = (String) jsonParser.get("question");
 		String[] answers = (String[]) jsonParser.get("answers");
@@ -88,11 +60,7 @@ public class Question {
 
 		return new Question(id, question, answers, solutionIndex, tags, owner);
 	}
-<<<<<<< HEAD
-	
-=======
 
->>>>>>> b617fb33fcd79d60a45f67126175b2dc04287de4
 	public long getId() {
 		return id;
 	}
