24255d8c03b2fa249af5286dc553ebc7b570fcbb
Merok <john.gaspoz@bluewin.ch>
2013-09-25 15:58:41 +0200

    add EditQuestionActivity

--- SwEng2013QuizApp/src/sweng/epfl/editquestions/EditQuestionActivity.java
+++ SwEng2013QuizApp/src/sweng/epfl/editquestions/EditQuestionActivity.java
@@ -1,13 +1,19 @@
 package sweng.epfl.editquestions;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 import epfl.sweng.R;
+import epfl.sweng.SubmitQuizzActivity;
+import epfl.sweng.showquestions.ShowQuestionsActivity;
 
 /**
  * The user can now enter a question that will be saved on a server.
  * @author born4new
+ * @author Merok
  *
  */
 public class EditQuestionActivity extends Activity {
@@ -16,6 +22,7 @@ public class EditQuestionActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_submit_question);
+		
 	}
 
 	@Override
@@ -24,5 +31,12 @@ public class EditQuestionActivity extends Activity {
 		getMenuInflater().inflate(R.menu.submit_question, menu);
 		return true;
 	}
+	
+	public void sendEditedQuestion(View view){
+		Toast.makeText(this, "Quizz submitted to the server.", Toast.LENGTH_SHORT).show();
+		Intent submitQuizzActivityIntent = new Intent(this, SubmitQuizzActivity.class);
+		startActivity(submitQuizzActivityIntent);
+	}
+	
 
 }
