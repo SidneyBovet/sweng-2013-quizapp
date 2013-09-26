f84aee86e9a29e3cc6efd44f7690e26a6ddb35d6
Merok <john.gaspoz@bluewin.ch>
2013-09-26 10:03:21 +0200

    add function in order to correctly format the elements of the QuizzEdit for submit

--- SwEng2013QuizApp/src/sweng/epfl/editquestions/EditQuestionActivity.java
+++ SwEng2013QuizApp/src/sweng/epfl/editquestions/EditQuestionActivity.java
@@ -1,8 +1,10 @@
 package sweng.epfl.editquestions;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -46,21 +48,59 @@ public class EditQuestionActivity extends Activity {
 		Toast.makeText(this, "Quizz submitted to the server.",
 				Toast.LENGTH_SHORT).show();
 
-		// this is done in order to get the content of the EditText elements in the XML
-		// WARNING : the order MUST BE : Question1 => Answer1 => ... => indexOfAnswer => tags
+		// this is done in order to get the content of the EditText elements in
+		// the XML
+		// WARNING : you MUST follow this structure when you write the EditText 
+		//elements : Question1 => Answer1 => ... => indexOfAnswer => tags
 		ArrayList<String> listElem = new ArrayList<String>();
-		for (int i = 0; i < layout.getChildCount(); i++){
+		for (int i = 0; i < layout.getChildCount(); i++) {
 			if (layout.getChildAt(i) instanceof EditText) {
 				EditText currentEditText = (EditText) layout.getChildAt(i);
 				String currentArgument = currentEditText.getText().toString();
 				listElem.add(currentArgument);
-			}	
+			}
 		}
 		
 
+		generatePostentity(listElem);
+		/*
+		 * post.setEntity(new StringEntity("{" +
+		 * " \"question\": \"What is the answer to life, the universe and everything?\","
+		 * + " \"answers\": [ \"42\", \"24\" ]," + " \"solutionIndex\": 0," +
+		 * " \"tags\": [ \"h2g2\", \"trivia\" ]" + " }"));
+		 * post.setHeader("Content-type", "application/json");
+		 * ResponseHandler<String> handler = new BasicResponseHandler(); String
+		 * response = SwengHttpClientFactory.getInstance().execute(post,
+		 * handler);
+		 */
+
+	}
+
+	private void generatePostentity(ArrayList<String> listElem) {
 		HttpPost post = new HttpPost(SERVER_URL + "/quizquestions/");
+		// We need to split the last element of the ArrayList in order to have
+		// the the tags separated
+		// tagA,tab2?tag3-tag5 => \"tagA\",\"tab2\",\"tag3\",\"tag5\" 
+		String tagsInOneLine = listElem.get(listElem.size() - 1);
+		String formattedTags = tagsInOneLine.replaceAll("\\W", "\", \"");
+		formattedTags = "\""+formattedTags+"\"";
+//		String formattedTags = tagsInOneLine.replaceAll(regularExpression, replacement)
+//		String[] tags = tagsInOneLine.split("\\W");
+		
 		
 
+		String questionJsonFormat = " \"question\":" + "\""
+				+ listElem.remove(0) + "\",";
+		String answersJsonFormat = " \"answers\": [ \"42\", \"24\" ],";
+	
+		try {
+			post.setEntity(new StringEntity("{" + questionJsonFormat
+					+ " \"answers\": [ \"" + 42 + "\", \"24\" ],"
+					+ " \"solutionIndex\": 0,"
+					+ " \"tags\": [ \"h2g2\", \"trivia\" ]" + " }"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
