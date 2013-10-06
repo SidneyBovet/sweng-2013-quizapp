55e4f57902b5d00eafe80b8e7836c885b7cd7cbc
Aymeric Genet <aymeric.genet@epfl.ch>
2013-10-06 16:40:43 +0200

    Revert "Remove QuizEditExecution from backend package."
    This reverts commit e98e5d81c5c2d2d1ad34bfd77dee22e14cf9d040.

---
+++ SwEng2013QuizApp/src/epfl/sweng/backend/QuizEditExecution.java
@@ -0,0 +1,40 @@
+package epfl.sweng.backend;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+import epfl.sweng.servercomm.SwengHttpClientFactory;
+
+/**
+ * 
+ * @author Merok
+ * 
+ */
+public class QuizEditExecution extends AsyncTask<JSONObject, Void, Void> {
+	private final static String SERVER_URL = "https://sweng-quiz.appspot.com";
+
+	@Override
+	protected Void doInBackground(JSONObject... jsonObject) {
+		HttpPost post = new HttpPost(SERVER_URL + "/quizquestions/");
+		// send the quiz
+		ResponseHandler<String> handler = new BasicResponseHandler();
+		try {
+			post.setEntity(new StringEntity(jsonObject[0].toString()));
+			post.setHeader("Content-type", "application/json");
+			SwengHttpClientFactory.getInstance().execute(post, handler);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+}
