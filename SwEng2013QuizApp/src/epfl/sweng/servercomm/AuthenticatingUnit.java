6cdb3b5b35f405a37c0f143b30a2d050856c2cc7
Sidney Bovet <sidney.bovet@epfl.ch>
2013-10-10 18:30:52 +0200

    Add AuthenticatingUnit.java as starting point for HW3 part 3.1

---
+++ SwEng2013QuizApp/src/epfl/sweng/servercomm/AuthenticatingUnit.java
@@ -0,0 +1,13 @@
+package epfl.sweng.servercomm;
+
+/**
+ * Singleton class handling all the authenticating process with EPFL's Tequila
+ * server.
+ * 
+ * @author Sidney
+ * 
+ */
+public class AuthenticatingUnit {
+	
+	
+}
