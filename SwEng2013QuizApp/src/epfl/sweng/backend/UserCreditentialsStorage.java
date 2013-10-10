249d421bd95b4669608f7d3ca67e9b88d5f2cd8f
Sidney Bovet <sidney.bovet@epfl.ch>
2013-10-10 18:32:01 +0200

    Add Double-Checked Singleton in order to avoid costly synchronized call each time getInstance() is called

--- SwEng2013QuizApp/src/epfl/sweng/backend/UserCreditentialsStorage.java
+++ SwEng2013QuizApp/src/epfl/sweng/backend/UserCreditentialsStorage.java
@@ -1,13 +1,15 @@
 package epfl.sweng.backend;
 
+import epfl.sweng.servercomm.AuthenticatingUnit;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 
 /**
  * Storage structure which use a SharedPreferences to store the sessionID
+ * 
  * @author JoTearoom
- *
+ * 
  */
 public final class UserCreditentialsStorage {
 	private static UserCreditentialsStorage singletonStorage;
@@ -20,66 +22,82 @@ public final class UserCreditentialsStorage {
 	// XXX qu'une key et qu'une sessionID = value?
 
 	/**
-	 * Constructor which job is to create the storage (using SharePreferences 
+	 * Constructor which job is to create the storage (using SharePreferences
 	 * interface)
+	 * 
 	 * @param context
-	 * 				context of the activity that creates the storage
+	 *            context of the activity that creates the storage
 	 */
 	private UserCreditentialsStorage(Context context) {
 		this.userCreditentialsPrefs = context.getSharedPreferences(
 				sharedPreferencesName, 0);
 		/*
-		 * Operating mode. Use 0 or MODE_PRIVATE for the default operation, 
+		 * Operating mode. Use 0 or MODE_PRIVATE for the default operation,
 		 * MODE_WORLD_READABLE and MODE_WORLD_WRITEABLE to control permissions.
-		 *  The bit MODE_MULTI_PROCESS can also be used if multiple processes 
-		 *  are mutating the same SharedPreferences file. MODE_MULTI_PROCESS is
-		 *   always on in apps targetting Gingerbread (Android 2.3) and below,
-		 *    and off by default in later versions.
+		 * The bit MODE_MULTI_PROCESS can also be used if multiple processes are
+		 * mutating the same SharedPreferences file. MODE_MULTI_PROCESS is
+		 * always on in apps targetting Gingerbread (Android 2.3) and below, and
+		 * off by default in later versions.
 		 */
 		this.editor = userCreditentialsPrefs.edit();
 	}
 
 	/**
 	 * Creation of the singleton
+	 * 
 	 * @param context
 	 * @return Singleton instance of the class
 	 */
 	public static UserCreditentialsStorage getSingletonInstanceOfStorage(
 			Context context) {
+		// double-checked singleton: avoids calling costly synchronized if
+		// unnecessary
 		if (null == singletonStorage) {
-			singletonStorage = new UserCreditentialsStorage(context);
+			synchronized (AuthenticatingUnit.class) {
+				if (null == singletonStorage) {
+					singletonStorage = new UserCreditentialsStorage(context);
+				}
+			}
 		}
 		return singletonStorage;
 	}
-	
+
 	/**
 	 * Put the sessionID in the storage table
-	 * @param sessionID that the user received from Tequila
+	 * 
+	 * @param sessionID
+	 *            that the user received from Tequila
 	 */
 	public void takeAuthentification(int sessionID) {
 		editor.putInt(keySessionIDName, sessionID);
-		/* Once you have changed the value you have to call the apply() method to 
-		apply your asynchronously to the file system. The usage of the commit()
-		method is discouraged, as it write the changes synchronously 
-		to the file system.*/
+		/*
+		 * Once you have changed the value you have to call the apply() method
+		 * to apply your asynchronously to the file system. The usage of the
+		 * commit() method is discouraged, as it write the changes synchronously
+		 * to the file system.
+		 */
 		editor.commit();
 	}
 
 	/**
 	 * Check if the sessionID belongs to the table or not
-	 * @param sessionID that is checked to be in the table.
+	 * 
+	 * @param sessionID
+	 *            that is checked to be in the table.
 	 * @return yes if the sessionID is already in the table
 	 */
 	public boolean isAuthentificated(int sessionID) {
-		//XXX defvalue int -1 or 0?
+		// XXX defvalue int -1 or 0?
 		int value = userCreditentialsPrefs.getInt(keySessionIDName, -1);
-		//XXX revient au meme que value!=-1?
-		return value==sessionID; 
+		// XXX revient au meme que value!=-1?
+		return value == sessionID;
 	}
 
 	/**
 	 * Remove the sessionID from the storage table
-	 * @param sessionID that we want to remove because the user logged out
+	 * 
+	 * @param sessionID
+	 *            that we want to remove because the user logged out
 	 */
 	public void releaseAuthentification(int sessionID) {
 		editor.remove(keySessionIDName);
