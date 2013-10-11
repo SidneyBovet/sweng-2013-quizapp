d3342d0693b2b9e19c874dd12cca184c14cf5c97
Merok <john.gaspoz@epfl.ch>
2013-10-11 13:19:58 +0200

    add javadoc to AuthenticationActivity class

--- SwEng2013QuizApp/src/epfl/sweng/testing/TestingTransactions.java
+++ SwEng2013QuizApp/src/epfl/sweng/testing/TestingTransactions.java
@@ -23,25 +23,28 @@ import android.util.Log;
  * TestingTransactions.check(TTChecks.THE_THING_I_JUST_DID) whenever a request
  * is completely done.
  */
-public class TestingTransactions {
-	private static final TestingTransactions instance = new TestingTransactions();
+public final class TestingTransactions {
+	private static final TestingTransactions INSTANCE = new TestingTransactions();
 
-	// / The tag used for logging
+	/** The tag used for logging */
 	private static final String TAG = "TestingTransaction";
 
-	// / The maximum time we wait for a transaction to complete, in milliseconds
+	/** The maximum time we wait for a transaction to complete, in milliseconds */
 	private static final long TRANSACTION_TIMEOUT = 5500;
 
-	// / The time when the current transaction was started
+	/** The time when the current transaction was started */
 	private long startTime;
 
-	// / The state of this transaction
+	/** The possible states of a transaction */
 	private enum TTState {
 		IDLE, INITIATED, COMPLETED
 	};
-
 	private TTState state = TTState.IDLE;
 
+	/**
+	 * The possible points at which transactions end. Each of these corresponds
+	 * to some finished action.
+	 */
 	public enum TTChecks {
 		NONE,
 		QUESTION_SHOWN,
@@ -68,9 +71,8 @@ public class TestingTransactions {
 			synchronized (tts) {
 				if (tts.state != TTState.IDLE) {
 					throw new TestingTransactionsError(
-							"Attempt to run transaction '"
-							+ t
-							+"', but another transaction is running.");
+							"Attempt to run transaction '" + t +
+							"', but another transaction is running.");
 				}
 				tts.startTime = System.currentTimeMillis();
 				Log.d(TAG, String.format("Starting transaction %s", t));
@@ -92,9 +94,8 @@ public class TestingTransactions {
 				// simultaneously, and set the state back to IDLE.
 				if (tts.state != TTState.INITIATED && tts.state != TTState.COMPLETED) {
 					throw new TestingTransactionsError(
-							"Attempt to wait for transaction '"
-							+ t
-							+ "', but it was aborted.");
+							"Attempt to wait for transaction '" + t +
+							"', but it was aborted.");
 				}
 				
 				// 2) wait for the transaction to complete (i.e., to call check)
@@ -161,6 +162,6 @@ public class TestingTransactions {
 
 	// Retrieve the singleton instance of TestingTransaction
 	private static TestingTransactions getInstance() {
-		return instance;
+		return INSTANCE;
 	}
 }
--- SwEng2013QuizAppTest/src/epfl/sweng/test/AuthenticationActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/AuthenticationActivityTest.java
@@ -1,18 +1,73 @@
 package epfl.sweng.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import com.jayway.android.robotium.solo.Solo;
 import epfl.sweng.entry.AuthenticationActivity;
+import epfl.sweng.testing.TestingTransaction;
+import epfl.sweng.testing.TestingTransactions;
+import epfl.sweng.testing.TestingTransactions.TTChecks;
 
 public class AuthenticationActivityTest extends
 		ActivityInstrumentationTestCase2<AuthenticationActivity> {
 
+	private Solo solo;
+
 	public AuthenticationActivityTest() {
 		super(AuthenticationActivity.class);
-
 	}
 
-	public void testhello() {
-		assertEquals("a", "a");
+	@Override
+	protected void setUp() {
+		solo = new Solo(getInstrumentation());
 	}
 
+	public void testShowEditTextAndLoginButton() {
+		getActivityAndWaitFor(TTChecks.AUTHENTICATION_ACTIVITY_SHOWN);
+		assertTrue("Username asked", solo.searchText("GASPAR Username"));
+		assertTrue("Password Asked", solo.searchText("GASPAR Password"));
+		assertTrue("Login Button is present",
+				solo.searchButton("Log in using Tequila"));
+	}
+
+	public void testLoginButtonIsDisabledAtBeggining() {
+		getActivityAndWaitFor(TTChecks.AUTHENTICATION_ACTIVITY_SHOWN);
+		Button loginButton = solo.getButton("Log in using Tequila");
+		assertFalse(loginButton.isEnabled());
+	}
+
+	// XXX HOW TO DO A TEST LIKE THAT ?
+//	public void testLoginButtonIsEnableWhenLoginAndPasswordFieldsAreCorrectlyCompleted() {
+//		getActivityAndWaitFor(TTChecks.AUTHENTICATION_ACTIVITY_SHOWN);
+//		EditText login = (EditText) solo.getEditText(id.username_login);
+//		EditText password = (EditText) solo.getEditText(id.password_login);
+//		Button loginButton = (Button) solo.getButton(id.buttonLogin);
+//		login.setText("    ");
+//		password.setText("     ");
+//		assertFalse("login and password fileds must be correctly filled", loginButton.isEnabled());
+//	}
+	
+	
+
+	private void getActivityAndWaitFor(
+			final TestingTransactions.TTChecks expected) {
+		TestingTransactions.run(getInstrumentation(), new TestingTransaction() {
+			@Override
+			public void initiate() {
+				getActivity();
+			}
+
+			@Override
+			public void verify(TestingTransactions.TTChecks notification) {
+				assertEquals(String.format(
+						"Expected notification %s, but received %s", expected,
+						notification), expected, notification);
+			}
+
+			@Override
+			public String toString() {
+				return String.format("getActivityAndWaitFor(%s)", expected);
+			}
+		});
+	}
 }
