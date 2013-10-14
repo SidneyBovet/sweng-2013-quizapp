85e20933bce2e61d5d97adb4c7e4ee4159778897
Merok <john.gaspoz@epfl.ch>
2013-10-14 22:33:42 +0200

    modify QuizEditExecution in order to test the class in order to get the result of the HTTP post

--- SwEng2013QuizApp/src/epfl/sweng/servercomm/QuizEditExecution.java
+++ SwEng2013QuizApp/src/epfl/sweng/servercomm/QuizEditExecution.java
@@ -3,6 +3,7 @@ package epfl.sweng.servercomm;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -14,31 +15,56 @@ import epfl.sweng.testing.TestCoordinator;
 import epfl.sweng.testing.TestCoordinator.TTChecks;
 
 /**
- * Asyncronous task that runs a background thread that sends a new {@link 
- * Question} to the SwEng server.
+ * Asyncronous task that runs a background thread that sends a new
+ * {@link Question} to the SwEng server.
  * 
  * @author Merok
  * 
  */
 public class QuizEditExecution extends AsyncTask<JSONObject, Void, Void> {
 	private final static String SERVER_URL = "https://sweng-quiz.appspot.com";
+	private HttpResponse response = null;
+
+	// @Override
+	// protected Void doInBackground(JSONObject... jsonObject) {
+	// HttpPost post = new HttpPost(SERVER_URL + "/quizquestions/");
+	// // send the quiz
+	// ResponseHandler<String> handler = new BasicResponseHandler();
+	// try {
+	// post.setEntity(new StringEntity(jsonObject[0].toString()));
+	// post.setHeader("Content-type", "application/json");
+	// SwengHttpClientFactory.getInstance().execute(post, handler);
+	// } catch (UnsupportedEncodingException e) {
+	// e.printStackTrace();
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	//
+	// TestCoordinator.check(TTChecks.NEW_QUESTION_SUBMITTED);
+	// return null;
+	// }
 
 	@Override
 	protected Void doInBackground(JSONObject... jsonObject) {
 		HttpPost post = new HttpPost(SERVER_URL + "/quizquestions/");
 		// send the quiz
-		ResponseHandler<String> handler = new BasicResponseHandler();
 		try {
 			post.setEntity(new StringEntity(jsonObject[0].toString()));
 			post.setHeader("Content-type", "application/json");
-			SwengHttpClientFactory.getInstance().execute(post, handler);
+			response = SwengHttpClientFactory.getInstance().execute(post);
+			System.out.println("KSJDKJSKD => "
+					+ response.getStatusLine().getStatusCode());
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		TestCoordinator.check(TTChecks.NEW_QUESTION_SUBMITTED);
 		return null;
+	}
+
+	public int getResponseStatus() {
+		return response.getStatusLine().getStatusCode();
 	}
 }
--- SwEng2013QuizAppTest/src/epfl/sweng/test/QuizEditExecutionTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/QuizEditExecutionTest.java
@@ -3,6 +3,9 @@ package epfl.sweng.test;
 import java.util.ArrayList;
 
 import org.apache.http.HttpResponse;
+import org.json.JSONException;
+
+import android.util.Log;
 
 import junit.framework.TestCase;
 import epfl.sweng.servercomm.QuizEditExecution;
@@ -16,12 +19,12 @@ public class QuizEditExecutionTest extends TestCase {
 	private int index = -100;
 
 	public QuizEditExecutionTest() {
-
 		mAnswers.add("reponse1");
 		mAnswers.add("      "); // Blank is an error.
 		mTags.add("**((/_:_:_");
 		mMockJson = new MockJSON(id, "ma question", mAnswers, index, mTags,
 				"BOB");
+
 	}
 
 	public void testBadRequestWhenBadJSON() {
@@ -29,7 +32,8 @@ public class QuizEditExecutionTest extends TestCase {
 		quizEditExecute.execute(mMockJson);
 		// HOW TO TEST IF THE RESULT OF THE REQUEST IS BAD ? DO I NEED TO CHANGE
 		// THE CODE IN QuizEditExecution ?
-		
 
+		// 401 is the status number of bad request ?
+		assertEquals(true, true);
 	}
 }
