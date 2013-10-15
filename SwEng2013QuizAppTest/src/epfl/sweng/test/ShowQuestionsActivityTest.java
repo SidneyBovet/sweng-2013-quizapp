3fb89b84f4759dcbda78911e58db6f4e09a9dee0
Sidney Bovet <sidney.bovet@epfl.ch>
2013-10-15 20:29:17 +0200

    Example of how to make mockClient to throw an exception + unhandled case (TO BE CORRECTED!)

--- SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
@@ -1,15 +1,32 @@
 package epfl.sweng.test;
+import java.io.IOException;
+
 import android.test.ActivityInstrumentationTestCase2;
+import epfl.sweng.servercomm.SwengHttpClientFactory;
 import epfl.sweng.showquestions.ShowQuestionsActivity;
+import epfl.sweng.test.minimalmock.MockHttpClient;
+import epfl.sweng.testing.TestCoordinator.TTChecks;
 
-public class ShowQuestionsActivityTest extends
-		ActivityInstrumentationTestCase2<ShowQuestionsActivity> {
-
+public class ShowQuestionsActivityTest extends GUITest<ShowQuestionsActivity> {
+	
+	private MockHttpClient mockClient;
+	
 	public ShowQuestionsActivityTest() {
 		super(ShowQuestionsActivity.class);
 	}
 	
-	public void testHello() {
-		fail("Not yet implemented");
-	}	
+	@Override
+	public void setUp() {
+		mockClient = new MockHttpClient();
+		SwengHttpClientFactory.setInstance(mockClient);
+	}
+	
+	public void testErrorWhileFetchingQuestionIsHandled() {
+		IOException ioe = new IOException();
+		mockClient.setIOExceptionToThrow(ioe );
+		
+		getActivityAndWaitFor(TTChecks.QUESTION_SHOWN);
+		
+		assert true;
+	}
 }
