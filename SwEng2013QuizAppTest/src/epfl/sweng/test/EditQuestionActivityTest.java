6677f16227537f89c8f36066c0b757435694eac9
Sidney Bovet <sidney.bovet@epfl.ch>
2013-10-15 22:14:16 +0200

    rmove the backslash, we're not in xml...

--- SwEng2013QuizAppTest/src/epfl/sweng/test/EditQuestionActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/EditQuestionActivityTest.java
@@ -20,11 +20,11 @@ public class EditQuestionActivityTest extends GUITest<EditQuestionActivity> {
 	public void testBasicsElementsAreHere() {
 		getActivityAndWaitFor(TTChecks.EDIT_QUESTIONS_SHOWN);
 		assertTrue("Question filed needed",
-				getSolo().searchText("Type in the question\'s tags"));
+				getSolo().searchText("Type in the question's tags"));
 		assertTrue("Question filed needed",
-				getSolo().searchText("Type in the question\'s text body"));
+				getSolo().searchText("Type in the question's text body"));
 		assertTrue("submit button needed", getSolo().searchButton("Submit"));
-		assertTrue("Answer must be false", getSolo().searchButton("✘˜"));
+		assertTrue("Answer must be false", getSolo().searchButton("✘"));
 		assertTrue("Answer must be a - minus question button", getSolo()
 				.searchButton("-"));
 		assertTrue("Answer must be a - add question button", getSolo()
