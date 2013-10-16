4a9d0ece9876b15d27054943dbfb5697e8b86739
born4new <david.dieulivol@gmail.com>
2013-10-16 03:37:34 +0200

    Correcting checkstyle + testing hooks

--- SwEng2013QuizApp/src/epfl/sweng/servercomm/QuizEditExecution.java
+++ SwEng2013QuizApp/src/epfl/sweng/servercomm/QuizEditExecution.java
@@ -44,17 +44,17 @@ public class QuizEditExecution extends AsyncTask<JSONObject, Void, Integer> {
 			response = SwengHttpClientFactory.getInstance().execute(post);
 			responseStatus = response.getStatusLine().getStatusCode();
 		} catch (UnsupportedEncodingException e) {
-			// TODO: Error handling
-			Log.e(this.getClass().getName(), "doInBackground(): StringEntity " +
-					"couldn't be instantied.", e);
+			// TODO Error handling
+			Log.e(this.getClass().getName(), "doInBackground(): StringEntity "
+					+ "couldn't be instantied.", e);
 		} catch (ClientProtocolException e) {
-			// TODO: Error handling
-			Log.e(this.getClass().getName(), "doInBackground(): Error with " +
-					"the HTTP protocol.", e);
+			// TODO Error handling
+			Log.e(this.getClass().getName(), "doInBackground(): Error with "
+					+ "the HTTP protocol.", e);
 		} catch (IOException e) {
-			// TODO: Error handling
-			Log.e(this.getClass().getName(), "doInBackground(): An I/O error " +
-					"has occurred.", e);
+			// TODO Error handling
+			Log.e(this.getClass().getName(), "doInBackground(): An I/O error "
+					+ "has occurred.", e);
 		}
 
 		return responseStatus;
