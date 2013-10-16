5d2f2fd3f3bd318a571631155e478fea873f2ad1
Joanna Salathe <joanna.salathe@epfl.ch>
2013-10-16 16:48:29 +0200

    Test for show question activity now correct, remove the one from sidney

--- SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
@@ -3,6 +3,7 @@ package epfl.sweng.test;
 import org.apache.http.HttpStatus;
 
 import android.widget.Button;
+import android.widget.ListView;
 import epfl.sweng.servercomm.SwengHttpClientFactory;
 import epfl.sweng.showquestions.ShowQuestionsActivity;
 import epfl.sweng.test.minimalmock.MockHttpClient;
@@ -23,14 +24,12 @@ public class ShowQuestionsActivityTest extends GUITest<ShowQuestionsActivity> {
     @Override
     public void setUp() {
         super.setUp();
-
         mockClient = new MockHttpClient();
         SwengHttpClientFactory.setInstance(mockClient);
-
     }
 
     public void testFetchQuestion() {
-        mockClient.pushCannedResponse(
+    	mockClient.pushCannedResponse(
                 "GET (?:https?://[^/]+|[^/]+)?/+quizquestions/random\\b",
                 HttpStatus.SC_OK,
                 "{\"question\": \"What is the answer to life, the universe, and everything?\","
@@ -43,8 +42,6 @@ public class ShowQuestionsActivityTest extends GUITest<ShowQuestionsActivity> {
                 getSolo().searchText("What is the answer to life, the universe, and everything?"));
         assertTrue("Correct answer must be displayed", getSolo().searchText("Forty-two"));
         assertTrue("Incorrect answer must be displayed", getSolo().searchText("Twenty-seven"));
-        Button nextQuestionButton = getSolo().getButton("Next question");
-        assertFalse("Next question button is disabled", nextQuestionButton.isEnabled());
     }
     
     //TODO find why clickontext does not work
@@ -60,18 +57,21 @@ public class ShowQuestionsActivityTest extends GUITest<ShowQuestionsActivity> {
         getActivityAndWaitFor(TTChecks.QUESTION_SHOWN);
         assertTrue("Correct answer must be displayed", getSolo().
         		searchText("Forty-two"));
-        getSolo().clickOnText("Forty-two");
-        //assertTrue("Correct answer has been selected and",
-        	//	getSolo().searchText("Forty-two ✔"));
     }
     
-	//TODO this test do not pass, please the one who create it fix it
-	/*public void testErrorWhileFetchingQuestionIsHandled() {
-		IOException ioe = new IOException();
-		//TODO merge failed, I cannot find the following method: JoTearoom
-		//mockClient.setIOExceptionToThrow(ioe);
-		getActivityAndWaitFor(TTChecks.QUESTION_SHOWN);
-		assert true;
-	}*/
+    public void testNextButtonFirstDisabled() {
+        mockClient.pushCannedResponse(
+                "GET (?:https?://[^/]+|[^/]+)?/+quizquestions/random\\b",
+                HttpStatus.SC_OK,
+                "{\"question\": \"What is the answer to life, the universe, and everything?\","
+                + " \"answers\": [\"Forty-two\", \"Twenty-seven\"], \"owner\": \"sweng\","
+                + " \"solutionIndex\": 0, \"tags\": [\"h2g2\", \"trivia\"], \"id\": \"1\" }",
+                "application/json");
+        getActivityAndWaitFor(TTChecks.QUESTION_SHOWN);
+        assertTrue("Button next is present", getSolo().
+        		searchText("Next question"));
+		Button nextButton = getSolo().getButton("Next question");
+		assertFalse(nextButton.isEnabled());
+    }    
 }
 
