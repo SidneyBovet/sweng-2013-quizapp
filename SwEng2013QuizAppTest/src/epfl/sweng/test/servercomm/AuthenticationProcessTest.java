4fd59ea517cd9d78c6e6facda1c974266cdcb40f
born4new <david.dieulivol@gmail.com>
2013-10-20 17:19:55 +0200

    In the middle of AuthenticationProcessTest

--- SwEng2013QuizAppTest/src/epfl/sweng/test/servercomm/AuthenticationProcessTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/servercomm/AuthenticationProcessTest.java
@@ -2,19 +2,34 @@ package epfl.sweng.test.servercomm;
 
 import java.util.concurrent.ExecutionException;
 
-import epfl.sweng.authentication.AuthenticationActivity;
-import epfl.sweng.servercomm.AuthenticationProcess;
-import epfl.sweng.test.activities.GUITest;
+import org.apache.http.HttpStatus;
 
-public class AuthenticationProcessTest extends GUITest<AuthenticationActivity> {
+import android.content.Context;
+import android.test.ActivityInstrumentationTestCase2;
+import epfl.sweng.authentication.AuthenticationActivity;
+import epfl.sweng.authentication.UserCredentialsStorage;
+import epfl.sweng.servercomm.AuthenticationProcess;
+import epfl.sweng.servercomm.SwengHttpClientFactory;
+import epfl.sweng.test.minimalmock.MockHttpClient;
+
+public class AuthenticationProcessTest extends
+	ActivityInstrumentationTestCase2<AuthenticationActivity> {
+	
+	private Context contextOfActivity;
+	private UserCredentialsStorage persistentStorage;
+	private MockHttpClient mockClient;
 	
 	public AuthenticationProcessTest() {
 		super(AuthenticationActivity.class);
+		mockClient = new MockHttpClient();
+		contextOfActivity = getInstrumentation().getTargetContext();
+		SwengHttpClientFactory.setInstance(mockClient);
+		persistentStorage = UserCredentialsStorage.getInstance(contextOfActivity);
 	}
 	
 	@Override
 	public void setUp() {
-		
+		persistentStorage.releaseAuthentication();
 	}
 	
 	@Override
@@ -23,10 +38,27 @@ public class AuthenticationProcessTest extends GUITest<AuthenticationActivity> {
 	}
 	
 	public void testDoInBackground() {
-		AuthenticationProcess authProcess = new AuthenticationProcess(new AuthenticationActivity());
-		authProcess.execute("dieulivo", "XXXXX");
+
+		mockClient.pushCannedResponse(
+				"GET https://sweng-quiz.appspot.com/login ", HttpStatus.SC_OK,
+				"{\"token\": \"tooookkkeeeenn\"}", "application/json");
+		final int found = 302;
+		mockClient.pushCannedResponse(
+				"POST https://tequila.epfl.ch/cgi-bin/tequila/login", found,
+				"", "HttpResponse");
+		mockClient.pushCannedResponse(
+				"POST https://sweng-quiz.appspot.com/login", HttpStatus.SC_OK,
+				"{\"session\": \"SessssionID\"}", "application/json");
+
+		assertTrue("Must be logged in", persistentStorage.isAuthenticated());
+		
+		AuthenticationProcess authProcess = 
+				new AuthenticationProcess(contextOfActivity);
+		
+		authProcess.execute("mockUsername", "mockPassword");
+		
 		try {
-			System.out.println(authProcess.get());
+			assertTrue("Should be ", authProcess.get() != null);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
