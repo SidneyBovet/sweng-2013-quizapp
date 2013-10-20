67695a6b9780c0949f19ca59c8ce01318ca4fcc2
Merok <john.gaspoz@epfl.ch>
2013-10-20 18:56:45 +0200

    improve test in ServerInteractionsTest.java , try to do AuthenticationProcessTest.java but could not find how to do this.

--- SwEng2013QuizAppTest/src/epfl/sweng/test/activities/AuthenticationActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/activities/AuthenticationActivityTest.java
@@ -30,10 +30,10 @@ public class AuthenticationActivityTest extends GUITest<AuthenticationActivity> 
 		SwengHttpClientFactory.setInstance(mockClient);
 		persistentStorage = UserCredentialsStorage
 				.getInstance(contextOfAuthenticationActivity);
+		getActivityAndWaitFor(TTChecks.AUTHENTICATION_ACTIVITY_SHOWN);
 	}
 
 	public void testShowEditTextAndLoginButton() {
-		getActivityAndWaitFor(TTChecks.AUTHENTICATION_ACTIVITY_SHOWN);
 		assertTrue("Username asked", getSolo().searchText("GASPAR Username"));
 		assertTrue("Password Asked", getSolo().searchText("GASPAR Password"));
 		assertTrue("Login Button is present",
@@ -41,13 +41,11 @@ public class AuthenticationActivityTest extends GUITest<AuthenticationActivity> 
 	}
 
 	public void testLoginButtonIsDisabledAtBeggining() {
-		getActivityAndWaitFor(TTChecks.AUTHENTICATION_ACTIVITY_SHOWN);
 		Button loginButton = getSolo().getButton("Log in using Tequila");
 		assertFalse(loginButton.isEnabled());
 	}
 
 	public void testLoginButtonIsEnableWhenLoginAndPasswordFieldsAreCorrectlyCompleted() {
-		getActivityAndWaitFor(TTChecks.AUTHENTICATION_ACTIVITY_SHOWN);
 		EditText login = (EditText) getSolo().getEditText("GASPAR Username");
 		EditText password = (EditText) getSolo().getEditText("GASPAR Password");
 		Button loginButton = (Button) getSolo().getButton(
@@ -60,7 +58,6 @@ public class AuthenticationActivityTest extends GUITest<AuthenticationActivity> 
 	}
 
 	public void testAuthenticationActivityWorks() {
-		getActivityAndWaitFor(TTChecks.AUTHENTICATION_ACTIVITY_SHOWN);
 		persistentStorage.destroyAuthentication();
 		getSolo().sleep(1);
 
@@ -81,11 +78,10 @@ public class AuthenticationActivityTest extends GUITest<AuthenticationActivity> 
 		getSolo().enterText(password, "Alligator21");
 
 		getSolo().clickOnButton("Log in using Tequila");
-		final int slp = 1000;
-		getSolo().sleep(slp);
+		getSolo().sleep(1000);
 
 		assertTrue("Must be logged in", persistentStorage.isAuthenticated());
-		getSolo().sleep(slp);
+		getSolo().sleep(1000);
 		getSolo().sleep(1);
 		// getSolo().assertCurrentActivity("Must Be MainActivity",
 		// MainActivity.class);
--- SwEng2013QuizAppTest/src/epfl/sweng/test/servercomm/ServerInteractionsTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/servercomm/ServerInteractionsTest.java
@@ -5,45 +5,58 @@ import java.util.List;
 
 import epfl.sweng.exceptions.ServerSubmitFailedException;
 import epfl.sweng.servercomm.ServerInteractions;
+import epfl.sweng.servercomm.SwengHttpClientFactory;
+import epfl.sweng.test.minimalmock.MockHttpClient;
 import junit.framework.TestCase;
 
 public class ServerInteractionsTest extends TestCase {
-	
+	private MockHttpClient mMockClient;
+
 	@Override
 	public void setUp() {
-		
+		mMockClient = new MockHttpClient();
+		mMockClient.pushCannedResponse(
+				"(POST|GET) https://sweng-quiz.appspot.com/quizquestions/",
+				200, "", "HttpResponse");
+		mMockClient
+				.pushCannedResponse(
+						".+",
+						200,
+						"{\"id\": 17005,\"question\": \"What is the capital of Antigua and Barbuda?\",\"answers\": [\"Chisinau\"],\"solutionIndex\": 2,\"tags\": [\"capitals\"],\"owner\": \"sweng\"}",
+						"application/json");
+		SwengHttpClientFactory.setInstance(mMockClient);
 	}
-	
+
 	@Override
 	public void tearDown() {
-		
+
 	}
-	
+
 	public void testGetRandomQuestion() {
 		assertTrue(ServerInteractions.getRandomQuestion() != null);
 	}
-	
+
 	public void testSubmitQuestion() {
 		List<String> listInputGUI = new ArrayList<String>();
 
 		listInputGUI.add("Question");
-		
+
 		List<String> listAnswers = new ArrayList<String>();
 		listAnswers.add("Answer1");
 		listAnswers.add("Answer2");
-		
+
 		listInputGUI.addAll(listAnswers);
-		
+
 		int indexGoodAnswer = 1;
 		String indexGoodAnswerString = Integer.toString(indexGoodAnswer);
-		
+
 		listInputGUI.add(indexGoodAnswerString);
 		listInputGUI.add("tag1, tag2, tag3");
-		
+
 		try {
 			int serverReturn = ServerInteractions.submitQuestion(listInputGUI);
 			// XXX This was 400 at the time of writing.
-			assertEquals(serverReturn, 201);
+			assertEquals(serverReturn, 200);
 		} catch (ServerSubmitFailedException e) {
 			fail("An exception occured while doing the test.");
 		}
