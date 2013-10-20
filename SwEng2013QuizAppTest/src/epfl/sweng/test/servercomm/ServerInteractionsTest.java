a6ce02a58b6d5739986af80e99146cbdaf5c68cf
Merok <john.gaspoz@epfl.ch>
2013-10-20 23:57:31 +0200

    test add

--- SwEng2013QuizAppTest/src/epfl/sweng/test/servercomm/ServerInteractionsTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/servercomm/ServerInteractionsTest.java
@@ -15,9 +15,7 @@ public class ServerInteractionsTest extends TestCase {
 	@Override
 	public void setUp() {
 		mMockClient = new MockHttpClient();
-		mMockClient.pushCannedResponse(
-				"(POST|GET) https://sweng-quiz.appspot.com/quizquestions/",
-				200, "", "HttpResponse");
+
 		mMockClient
 				.pushCannedResponse(
 						".+",
@@ -33,10 +31,14 @@ public class ServerInteractionsTest extends TestCase {
 	}
 
 	public void testGetRandomQuestion() {
+
 		assertTrue(ServerInteractions.getRandomQuestion() != null);
 	}
 
 	public void testSubmitQuestion() {
+		mMockClient.pushCannedResponse(
+				"(POST|GET) https://sweng-quiz.appspot.com/quizquestions/",
+				200, "", "HttpResponse");
 		List<String> listInputGUI = new ArrayList<String>();
 
 		listInputGUI.add("Question");
@@ -57,6 +59,34 @@ public class ServerInteractionsTest extends TestCase {
 			int serverReturn = ServerInteractions.submitQuestion(listInputGUI);
 			// XXX This was 400 at the time of writing.
 			assertEquals(serverReturn, 200);
+		} catch (ServerSubmitFailedException e) {
+			fail("An exception occured while doing the test.");
+		}
+	}
+
+	public void testSubmitQuestionBadQuestion() {
+		mMockClient.pushCannedResponse(
+				"(POST|GET) https://sweng-quiz.appspot.com/quizquestions/",
+				400, "", "HttpResponse");
+		List<String> listInputGUI = new ArrayList<String>();
+
+		listInputGUI.add("Question");
+
+		List<String> listAnswers = new ArrayList<String>();
+		listAnswers.add("   ");
+		listAnswers.add("Answer2");
+
+		listInputGUI.addAll(listAnswers);
+
+		int indexGoodAnswer = 1;
+		String indexGoodAnswerString = Integer.toString(indexGoodAnswer);
+
+		listInputGUI.add(indexGoodAnswerString);
+		listInputGUI.add("tag1, tag2, tag3");
+
+		try {
+			int serverReturn = ServerInteractions.submitQuestion(listInputGUI);
+			assertEquals(serverReturn, 400);
 		} catch (ServerSubmitFailedException e) {
 			fail("An exception occured while doing the test.");
 		}
