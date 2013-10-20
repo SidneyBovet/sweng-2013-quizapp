c8a3ea68e6ae9b3acd24efc4eb6425869a8289b4
Merok <john.gaspoz@epfl.ch>
2013-10-20 23:42:32 +0200

    fix merge bug

--- SwEng2013QuizAppTest/src/epfl/sweng/test/JSONUploaderTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/JSONUploaderTest.java
@@ -5,10 +5,7 @@ import java.util.concurrent.ExecutionException;
 
 import junit.framework.TestCase;
 import epfl.sweng.servercomm.JSONUploader;
-<<<<<<< Updated upstream
-=======
 import epfl.sweng.servercomm.SwengHttpClientFactory;
->>>>>>> Stashed changes
 import epfl.sweng.test.minimalmock.MockHttpClient;
 
 public class JSONUploaderTest extends TestCase {
@@ -16,12 +13,8 @@ public class JSONUploaderTest extends TestCase {
 	private ArrayList<String> mAnswers = new ArrayList<String>();
 	private ArrayList<String> mTags = new ArrayList<String>();
 	private MockJSON mMockJson = null;
-<<<<<<< Updated upstream
-	private MockHttpClient mMockClient = new MockHttpClient();
-=======
 	private MockHttpClient mMockClient;
 
->>>>>>> Stashed changes
 	private final int id = -33;
 	private final int index = -100;
 
@@ -30,37 +23,17 @@ public class JSONUploaderTest extends TestCase {
 
 	@Override
 	protected void setUp() throws Exception {
-<<<<<<< Updated upstream
-=======
 		mMockClient = new MockHttpClient();
 		SwengHttpClientFactory.setInstance(mMockClient);
 		super.setUp();
 	}
 
 	public void testBadRequestIsHandled() {
->>>>>>> Stashed changes
 		mAnswers.add("reponse1");
 		mAnswers.add("     "); // Blank is an error.
 		mTags.add("**((/_:_:_");
 		mMockJson = new MockJSON(id, "ma question", mAnswers, index, mTags,
 				"BOB");
-<<<<<<< Updated upstream
-		
-		super.setUp();
-	}
-	
-// XXX C'est pas le but de tester si le serveur r�pond 400 ou pas mais si nous 
-//		on g�re le 400
-	
-	public void testBadRequestIsHandled() {
-
-		mMockClient.pushCannedResponse(
-				".+", ERROR_CODE, "", "");
-		JSONUploader quizEditExecute = new JSONUploader();
-		quizEditExecute.execute(mMockJson);
-		try {
-			quizEditExecute.get();
-=======
 		mMockClient.pushCannedResponse(".+", ERROR_CODE, "", "HttpResponse");
 		try {
 			JSONUploader quizEditExecute = new JSONUploader();
@@ -105,16 +78,12 @@ public class JSONUploaderTest extends TestCase {
 		int result = -1;
 		try {
 			result = quizEditExecute.get();
->>>>>>> Stashed changes
 		} catch (InterruptedException e) {
 			fail("What a Terrible Failure (aka. WTF!?");
 		} catch (ExecutionException e) {
 			fail("An exception was thrown while executing JSONUploader");
 		}
-<<<<<<< Updated upstream
-=======
 		assertTrue("Must be 200 since it's a OK result", result == 200);
 
->>>>>>> Stashed changes
 	}
 }
