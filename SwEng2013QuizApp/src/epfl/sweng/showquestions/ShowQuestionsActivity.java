3468bebabb7a6ad8993aa4e859b32e5c0698129d
Sidney Bovet <sidney.bovet@epfl.ch>
2013-11-05 19:19:52 +0100

    Correct a conception mistake

--- SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
@@ -128,10 +128,7 @@ public class ShowQuestionsActivity extends Activity {
 								R.string.error_fetching_question),
 						Toast.LENGTH_LONG).show();
 				
-				// We go back to the main Activity.
-				startActivity(new Intent(ShowQuestionsActivity.this, MainActivity.class));
 				finish();
-
 			}
 		}
 	}
