d08c48b37f3b23aec0937d376c7d3d7a0f4985d7
Joanna Salathe <joanna.salathe@epfl.ch>
2013-11-30 17:54:14 +0100

    Fix of the test singleton by adding a context when creating the proxy

--- SwEng2013QuizAppTest/src/epfl/sweng/test/proxy/QuestionProxyTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/proxy/QuestionProxyTest.java
@@ -5,24 +5,37 @@ import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 
+import android.content.Context;
+
 import junit.framework.TestCase;
+import epfl.sweng.entry.MainActivity;
 import epfl.sweng.patterns.QuestionsProxy;
 import epfl.sweng.quizquestions.QuizQuestion;
+import epfl.sweng.test.activities.GUITest;
 
-public class QuestionProxyTest extends TestCase{	
+public class QuestionProxyTest extends GUITest<MainActivity>{	
+	
+	public QuestionProxyTest() {
+		super(MainActivity.class);
+	}
+
 	private QuestionsProxy proxy;
 	private QuizQuestion mQuestion;
+	private Context contextOfMainActivity;
 	
 	@Override
-	protected void setUp() throws Exception {
-		proxy = QuestionsProxy.getInstance();
+	protected void setUp() {
+		super.setUp();
+		contextOfMainActivity = getInstrumentation()
+				.getTargetContext();
+		proxy = QuestionsProxy.getInstance(contextOfMainActivity);
 		mQuestion = new QuizQuestion("q", 
 				new ArrayList<String>(Arrays.asList("a1", "a2", "a3")), 
-				0, new TreeSet<String>(Arrays.asList("t1", "t2")),1, "o");
+				0, new TreeSet<String>(Arrays.asList("t1", "t2")), 1, "o");
 	}
 	
 	public void testSingleton() {
-		QuestionsProxy proxy2 = QuestionsProxy.getInstance();
+		QuestionsProxy proxy2 = QuestionsProxy.getInstance(contextOfMainActivity);
 		assertTrue(proxy.equals(proxy2));
 	} 
 
