791d2d9b152bfa195904bad9e1c63e1c5cc131e1
Aymeric Genet <aymeric.genet@epfl.ch>
2013-12-01 15:45:54 +0100

    Add a NetworkCommunicationTest (not implemented yet). It mainly covers the sad paths of the class.

---
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/servercomm/NetworkCommunicationTest.java
@@ -0,0 +1,58 @@
+package epfl.sweng.test.servercomm;
+
+import junit.framework.TestCase;
+
+public class NetworkCommunicationTest extends TestCase {
+
+	public void testSendQuizQuestionSuccessful() {
+		
+	}
+	
+	public void testSendQuizQuestionIOException() {
+		
+	}
+	
+	public void testSendQuizQuestionClientProtocolException() {
+		
+	}
+	
+	public void testSendQuizQuestionBaseServerError() {
+		
+	}
+	
+	public void testRetrieveQuizQuestionSuccessful() {
+		
+	}
+	
+	public void testRetrieveQuizQuestionIOException() {
+		
+	}
+	
+	public void testRetrieveQuizQuestionClientProtocolException() {
+		
+	}
+	
+	public void testRetrieveQuizQuestionExceptionBaseServerError() {
+		
+	}
+	
+	public void testRetrieveQuizQuestionWrongJSON() {
+		
+	}
+	
+	public void testSendQuizQuerySuccessful() {
+		
+	}
+	
+	public void testRetrieveQuizQueryClientProtocolException() {
+		
+	}
+	
+	public void testRetrieveQuizQueryIOException() {
+		
+	}
+	
+	public void testRetrieveQuizQueryWrongJSON() {
+		
+	}
+}
