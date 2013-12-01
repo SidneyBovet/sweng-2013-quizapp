c689630b6e229c990d4950d72d47bc81fc2c914e
Aymeric Genet <aymeric.genet@epfl.ch>
2013-12-01 15:21:12 +0100

    AuthenticationProcess mostly covered.

--- SwEng2013QuizAppTest/src/epfl/sweng/test/servercomm/AuthenticationProcessTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/servercomm/AuthenticationProcessTest.java
@@ -9,14 +9,14 @@ import epfl.sweng.authentication.AuthenticationActivity;
 import epfl.sweng.preferences.UserPreferences;
 import epfl.sweng.servercomm.AuthenticationProcess;
 import epfl.sweng.servercomm.SwengHttpClientFactory;
-import epfl.sweng.test.minimalmock.MockHttpClient;
+import epfl.sweng.test.minimalmock.AdvancedMockHttpClient;
 
 public class AuthenticationProcessTest extends
 		ActivityInstrumentationTestCase2<AuthenticationActivity> {
 
 	private Context contextOfActivity;
 	private UserPreferences persistentStorage;
-	private MockHttpClient mockClient;
+	private AdvancedMockHttpClient mockClient;
 
 	public AuthenticationProcessTest() {
 		super(AuthenticationActivity.class);
@@ -24,7 +24,7 @@ public class AuthenticationProcessTest extends
 
 	@Override
 	public void setUp() {
-		mockClient = new MockHttpClient();
+		mockClient = new AdvancedMockHttpClient();
 		contextOfActivity = getInstrumentation().getTargetContext();
 		SwengHttpClientFactory.setInstance(mockClient);
 		persistentStorage = UserPreferences
@@ -36,8 +36,8 @@ public class AuthenticationProcessTest extends
 	public void tearDown() {
 
 	}
-
-	public void testDoInBackground() {
+	
+	public void testAuthenticationSuccessful() {
 		mockClient.pushCannedResponse(
 				"GET https://sweng-quiz.appspot.com/login ", HttpStatus.SC_OK,
 				"{\"token\": \"tooookkkeeeenn\"}", "application/json");
@@ -48,33 +48,129 @@ public class AuthenticationProcessTest extends
 		mockClient.pushCannedResponse(
 				"POST https://sweng-quiz.appspot.com/login", HttpStatus.SC_OK,
 				"{\"session\": \"SessssionID\"}", "application/json");
-
-		//AuthenticationProcess authProcess = new AuthenticationProcess(
-			//	contextOfActivity);
-		//authProcess.execute("mockUsername", "mockPassword");
-
+		
 		String sessionID = AuthenticationProcess.
 				authenticate("mockUsername", "mockPassword");
 		assertEquals("SessssionID", sessionID);
+	}
+
+	public void testGetTokenWrongJSON() {
+		mockClient.pushCannedResponse(
+				"GET https://sweng-quiz.appspot.com/login ", HttpStatus.SC_OK,
+				"not a single structure of JSON token was here", "application/json");
 		
-		// TODO MUST ADD TIME IN ORDER TO LET THE PROCESS EXECUTE.
-		// XXX le problème ici est que l'on doit passer un context a
-		// AuthenticationProcess. Et je ne sais pas
-		// pourquoi lors d'un test si on passe le context , cela donne une
-		// erreur.
-
-		//assertTrue("Must be logged in", persistentStorage.isAuthenticated());
-
-		//
-		// authProcess.execute("mockUsername", "mockPassword");
-		//
-		// try {
-		// assertTrue("Should be ", authProcess.get() != null);
-		// } catch (InterruptedException e) {
-		// e.printStackTrace();
-		// } catch (ExecutionException e) {
-		// e.printStackTrace();
-		// }
+		String sessionID = AuthenticationProcess.
+				authenticate("mockUsername", "mockPassword");
+		
+		assertEquals("SessionID should be null.", sessionID, null);
+	}
+	
+	public void testGetTokenIOException() {
+		mockClient.pushCannedResponse(
+				".", AdvancedMockHttpClient.IOEXCEPTION_ERROR_CODE,
+				"", "", true);
+		
+		String sessionID = AuthenticationProcess.
+				authenticate("mockUsername", "mockPassword");
+		
+		assertEquals("SessionID should be null.", sessionID, null);
+	}
+	
+	public void testGetTokenClientProtocolException() {
+		mockClient.pushCannedResponse(
+				".", AdvancedMockHttpClient.CLIENTPROTOCOLEXCEPTION_ERROR_CODE,
+				"", "", true);
+		
+		String sessionID = AuthenticationProcess.
+				authenticate("mockUsername", "mockPassword");
+		
+		assertEquals("SessionID should be null.", sessionID, null);
+	}
+	
+	public void testValidateTokenClientProtocolException() {
+		mockClient.pushCannedResponse(
+				"GET https://sweng-quiz.appspot.com/login ", HttpStatus.SC_OK,
+				"{\"token\": \"tooookkkeeeenn\"}", "application/json");
+		mockClient.pushCannedResponse(
+				"POST https://tequila.epfl.ch/cgi-bin/tequila/login",
+				AdvancedMockHttpClient.CLIENTPROTOCOLEXCEPTION_ERROR_CODE,
+				"", "", true);
+		
+		String sessionID = AuthenticationProcess.
+				authenticate("mockUsername", "mockPassword");
+		
+		assertEquals("SessionID should be null.", sessionID, null);
+	}
+	
+	public void testValidateTokenIOException() {
+		mockClient.pushCannedResponse(
+				"GET https://sweng-quiz.appspot.com/login ", HttpStatus.SC_OK,
+				"{\"token\": \"tooookkkeeeenn\"}", "application/json");
+		mockClient.pushCannedResponse(
+				"POST https://tequila.epfl.ch/cgi-bin/tequila/login",
+				AdvancedMockHttpClient.IOEXCEPTION_ERROR_CODE,
+				"", "", true);
+		
+		String sessionID = AuthenticationProcess.
+				authenticate("mockUsername", "mockPassword");
+		
+		assertEquals("SessionID should be null.", sessionID, null);
+	}
+	
+	public void testSessionIDIOException() {
+		mockClient.pushCannedResponse(
+				"GET https://sweng-quiz.appspot.com/login ", HttpStatus.SC_OK,
+				"{\"token\": \"tooookkkeeeenn\"}", "application/json");
+		final int found = 302;
+		mockClient.pushCannedResponse(
+				"POST https://tequila.epfl.ch/cgi-bin/tequila/login", found,
+				"", "HttpResponse");
+		mockClient.pushCannedResponse(
+				"POST https://sweng-quiz.appspot.com/login",
+				AdvancedMockHttpClient.IOEXCEPTION_ERROR_CODE,
+				"", "", true);
+		
+		String sessionID = AuthenticationProcess.
+				authenticate("mockUsername", "mockPassword");
+		
+		assertEquals("SessionID should be null.", sessionID, null);
+	}
+	
+	public void testSessionIDClientProtocolException() {
+		mockClient.pushCannedResponse(
+				"GET https://sweng-quiz.appspot.com/login ", HttpStatus.SC_OK,
+				"{\"token\": \"tooookkkeeeenn\"}", "application/json");
+		final int found = 302;
+		mockClient.pushCannedResponse(
+				"POST https://tequila.epfl.ch/cgi-bin/tequila/login", found,
+				"", "HttpResponse");
+		mockClient.pushCannedResponse(
+				"POST https://sweng-quiz.appspot.com/login",
+				AdvancedMockHttpClient.CLIENTPROTOCOLEXCEPTION_ERROR_CODE,
+				"", "", true);
+		
+		String sessionID = AuthenticationProcess.
+				authenticate("mockUsername", "mockPassword");
+		
+		assertEquals("SessionID should be null.", sessionID, null);
+	}
+	
+	public void testSessionIDWrongJSON() {
+		mockClient.pushCannedResponse(
+				"GET https://sweng-quiz.appspot.com/login ", HttpStatus.SC_OK,
+				"{\"token\": \"tooookkkeeeenn\"}", "application/json");
+		final int found = 302;
+		mockClient.pushCannedResponse(
+				"POST https://tequila.epfl.ch/cgi-bin/tequila/login", found,
+				"", "HttpResponse");
+		mockClient.pushCannedResponse(
+				"POST https://sweng-quiz.appspot.com/login", HttpStatus.SC_OK,
+				"not a single structure of JSON sessionID was here", "application/json");
+		
+		String sessionID = AuthenticationProcess.
+				authenticate("mockUsername", "mockPassword");
+		
+		assertEquals("SessionID should be null.", sessionID, null);
 	}
 }
 
