d88eafad6d5487e29b83b769fef49fb48cf0b982
born4new <david.dieulivol@gmail.com>
2013-12-01 19:40:17 +0100

    fixes QuestionAgentFactoryTest

--- SwEng2013QuizAppTest/src/epfl/sweng/test/agent/QuestionAgentFactoryTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/agent/QuestionAgentFactoryTest.java
@@ -1,75 +1,74 @@
-/*package epfl.sweng.test.agent;
+package epfl.sweng.test.agent;
 
 import android.content.Context;
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
 import epfl.sweng.agents.CachedQuestionAgent;
 import epfl.sweng.agents.OnlineQuestionsAgent;
 import epfl.sweng.agents.QuestionAgent;
 import epfl.sweng.agents.QuestionAgentFactory;
 import epfl.sweng.backend.QuizQuery;
-import epfl.sweng.caching.CacheContentProvider;
 import epfl.sweng.patterns.ConnectivityState;
 import epfl.sweng.preferences.UserPreferences;
-import epfl.sweng.showquestions.ShowQuestionsActivity;
-import epfl.sweng.test.activities.GUITest;
 
-public class QuestionAgentFactoryTest extends GUITest<ShowQuestionsActivity> {
-	private QuizQuery fakeQueryOnline; 
+public class QuestionAgentFactoryTest extends AndroidTestCase {
+	private QuizQuery fakeQueryOnline;
 	private QuizQuery fakeQueryCache;
 	private CachedQuestionAgent fakeCachedAgent;
 	private Context contextShowQuestionActivity;
-	private CacheContentProvider mContentProvider;
-	
-	
-	public QuestionAgentFactoryTest() {
-		super(ShowQuestionsActivity.class);
-	}
 
 	@Override
 	protected void setUp() {
-		super.setUp();
-		contextShowQuestionActivity = getInstrumentation()
-				.getTargetContext();
-		mContentProvider = new CacheContentProvider(
-				contextShowQuestionActivity, true);
-		mContentProvider.eraseDatabase();
+		
+		try {
+			super.setUp();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		contextShowQuestionActivity = new RenamingDelegatingContext(
+				getContext(), "test_");
+		
 		fakeQueryOnline = new QuizQuery("queryOnline", "from");
 		fakeQueryCache = new QuizQuery("queryOffline", "from");
-		fakeCachedAgent = new CachedQuestionAgent(fakeQueryCache, 
+		fakeCachedAgent = new CachedQuestionAgent(fakeQueryCache,
 				contextShowQuestionActivity);
-
 	}
-	
+
 	@Override
 	protected void tearDown() throws Exception {
 		super.tearDown();
-		mContentProvider.close();
 		fakeCachedAgent.close();
 	}
-	
+
 	public void testInstance() {
 		QuestionAgentFactory.setInstance(fakeCachedAgent);
-		QuestionAgent agentToTest = 
-				QuestionAgentFactory.getAgent(contextShowQuestionActivity, fakeQueryCache);
+		QuestionAgent agentToTest = QuestionAgentFactory.getAgent(
+				contextShowQuestionActivity, fakeQueryCache);
 		assertEquals(fakeCachedAgent, agentToTest);
 		QuestionAgentFactory.releaseInstance();
 	}
-	
+
 	public void testGetAgent() {
-		QuestionAgent agentOfflineToTest = QuestionAgentFactory.
-				getAgent(contextShowQuestionActivity, fakeQueryCache);
+		
+		UserPreferences.getInstance(contextShowQuestionActivity)
+		.setConnectivityState(ConnectivityState.OFFLINE);
+		
+		QuestionAgent agentOfflineToTest = QuestionAgentFactory.getAgent(
+				contextShowQuestionActivity, fakeQueryCache);
 		assertTrue(agentOfflineToTest instanceof CachedQuestionAgent);
 		QuestionAgentFactory.releaseInstance();
-		
-		UserPreferences.getInstance(contextShowQuestionActivity).
-			setConnectivityState(ConnectivityState.ONLINE);
-		QuestionAgent agentOnlineToTest = QuestionAgentFactory.
-				getAgent(contextShowQuestionActivity, fakeQueryOnline);
+
+		UserPreferences.getInstance(contextShowQuestionActivity)
+				.setConnectivityState(ConnectivityState.ONLINE);
+		QuestionAgent agentOnlineToTest = QuestionAgentFactory.getAgent(
+				contextShowQuestionActivity, fakeQueryOnline);
 		assertTrue(agentOnlineToTest instanceof OnlineQuestionsAgent);
 		QuestionAgentFactory.releaseInstance();
-		
+
 		QuestionAgentFactory.setInstance(fakeCachedAgent);
-		QuestionAgent agentToTest = QuestionAgentFactory.
-				getAgent(contextShowQuestionActivity, fakeQueryCache);
+		QuestionAgent agentToTest = QuestionAgentFactory.getAgent(
+				contextShowQuestionActivity, fakeQueryCache);
 		assertTrue(agentToTest instanceof CachedQuestionAgent);
 	}
-}*/
+}
