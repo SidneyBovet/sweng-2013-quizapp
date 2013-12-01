7bc32b6cf1f6e51da1507f3d5b86d7bcb6d70f8a
Aymeric Genet <aymeric.genet@epfl.ch>
2013-12-01 15:44:15 +0100

    SwengHttpClientFactoryTest resets the client after creation.

--- SwEng2013QuizAppTest/src/epfl/sweng/test/servercomm/SwengHttpClientFactoryTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/servercomm/SwengHttpClientFactoryTest.java
@@ -11,5 +11,7 @@ public class SwengHttpClientFactoryTest extends TestCase {
 		AbstractHttpClient client = SwengHttpClientFactory.getInstance();
 		
 		assertNotNull("Client retrieved should not be null", client);
+		
+		SwengHttpClientFactory.setInstance(null);
 	}
 }
