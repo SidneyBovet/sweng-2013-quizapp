58451c3a88abbd15da4949189e6c5d8d3b2eb3ea
Sidney Bovet <sidney.bovet@epfl.ch>
2013-12-06 17:04:18 +0100

    sonarqube stuff (may be ignored later)

--- SwEng2013QuizApp/src/epfl/sweng/caching/CacheContentProvider.java
+++ SwEng2013QuizApp/src/epfl/sweng/caching/CacheContentProvider.java
@@ -292,7 +292,109 @@ public class CacheContentProvider {
 	}
 
 	public boolean isClosed() {
-		return !mDatabase.isOpen();
+		return null == mDatabase || !mDatabase.isOpen();
+	}
+	
+
+	/*********************** Search mechanism methods ***********************/
+
+	//pre-condition: # of '?' in normalizedTagList == questionSetList.size()
+	public Set<Long> evaluate(List<String> normalizedTagList, List<Set<Long>> questionsSetList) {
+		while (normalizedTagList.contains(")")) {
+			List<String> originalNormalizedTagList = new ArrayList<String>(normalizedTagList);
+			int firstClosingParenthesisIndex = normalizedTagList.indexOf(")");
+			int correspondingOpeningParenthesisIndex = 0;
+			
+			List<String> expressionParenthesized = new ArrayList<String>();
+			normalizedTagList.remove(firstClosingParenthesisIndex);
+			for (int i = firstClosingParenthesisIndex-1; i >= 0; i--) {
+				if (normalizedTagList.get(i).equals("(")) {
+					normalizedTagList.remove(i);
+					correspondingOpeningParenthesisIndex = i;
+					break;
+				} else {
+					expressionParenthesized.add(normalizedTagList.get(i));
+					normalizedTagList.remove(i);
+				}
+			}
+			Collections.reverse(expressionParenthesized);
+			// we now work to suppress the group between those two indexes
+			
+			List<Set<Long>> setListedParenthesized = getSubListedSet(
+					originalNormalizedTagList,
+					questionsSetList,
+					correspondingOpeningParenthesisIndex,
+					firstClosingParenthesisIndex);
+			
+			reduceGroup(expressionParenthesized, setListedParenthesized);
+			
+			normalizedTagList.add(correspondingOpeningParenthesisIndex, "?");
+		}
+		
+		return questionsSetList.get(0);
+	}
+
+	public List<Set<Long>> getSubListedSet(List<String> normalizedTagList,
+			List<Set<Long>> questionsSetList, int start, int end) {
+		int firstElementsCount = 0;
+		int parenthesizedCount = 0;
+		for (int i = 0; i < end; i++) {
+			if (normalizedTagList.get(i).equals("?")) {
+				if (i < start) {
+					firstElementsCount++;
+				} else {
+					parenthesizedCount++;
+				}
+			}
+		}
+		
+		return questionsSetList.subList(firstElementsCount, firstElementsCount
+				+ parenthesizedCount);
+	}
+
+	// pre-condition: tagList does not contain parenthesis and is well-formed
+	public Set<Long> reduceGroup(List<String> tagList, List<Set<Long>> questionsSetList) {
+		while (tagList.contains("*")) {
+			int firstANDIndex = tagList.indexOf("*");
+			
+			int leftOperandIndex = -1;
+			for (int i = 0; i < firstANDIndex; i++) {
+				if (tagList.get(i).equals("?")) {
+					leftOperandIndex++;
+				}
+			}
+	
+			// compute the INTERSECTION of the two operands
+			Set<Long> leftOperand = questionsSetList.get(leftOperandIndex);
+			Set<Long> rightOperand = questionsSetList.remove(leftOperandIndex+1);
+			leftOperand.retainAll(rightOperand);
+			
+			// update the tagList
+			tagList.remove(firstANDIndex);
+			tagList.remove(firstANDIndex); // this will remove the right-hand operand
+		}
+		
+	
+		while (tagList.contains("+")) {
+			int firstANDIndex = tagList.indexOf("+");
+			
+			int leftOperandIndex = -1;
+			for (int i = 0; i < firstANDIndex; i++) {
+				if (tagList.get(i).equals("?")) {
+					leftOperandIndex++;
+				}
+			}
+	
+			// compute the UNION of the two operands
+			Set<Long> leftOperand = questionsSetList.get(leftOperandIndex);
+			Set<Long> rightOperand = questionsSetList.remove(leftOperandIndex+1);
+			leftOperand.addAll(rightOperand);
+			
+			// update the tagList
+			tagList.remove(firstANDIndex);
+			tagList.remove(firstANDIndex); // this will remove the right-hand operand
+		}
+		return questionsSetList.get(0);
 	}
 
 	/*********************** Private methods ***********************/
@@ -593,104 +695,5 @@ public class CacheContentProvider {
 		statement = statement + ")";
 		statement = "(" + statement.substring(1);
 		return statement;
-	}
-	
-	//pre-condition: # of '?' in normalizedTagList == questionSetList.size()
-	public Set<Long> evaluate(List<String> normalizedTagList, List<Set<Long>> questionsSetList) {
-		while (normalizedTagList.contains(")")) {
-			List<String> originalNormalizedTagList = new ArrayList<String>(normalizedTagList);
-			int firstClosingParenthesisIndex = normalizedTagList.indexOf(")");
-			int correspondingOpeningParenthesisIndex = 0;
-			
-			List<String> expressionParenthesized = new ArrayList<String>();
-			normalizedTagList.remove(firstClosingParenthesisIndex);
-			for (int i = firstClosingParenthesisIndex-1; i >= 0; i--) {
-				if (normalizedTagList.get(i).equals("(")) {
-					normalizedTagList.remove(i);
-					correspondingOpeningParenthesisIndex = i;
-					break;
-				} else {
-					expressionParenthesized.add(normalizedTagList.get(i));
-					normalizedTagList.remove(i);
-				}
-			}
-			Collections.reverse(expressionParenthesized);
-			// we now work to suppress the group between those two indexes
-			
-			List<Set<Long>> setListedParenthesized = getSubListedSet(
-					originalNormalizedTagList,
-					questionsSetList,
-					correspondingOpeningParenthesisIndex,
-					firstClosingParenthesisIndex);
-			
-			reduceGroup(expressionParenthesized, setListedParenthesized);
-			
-			normalizedTagList.add(correspondingOpeningParenthesisIndex, "?");
-		}
-		
-		return questionsSetList.get(0);
-	}
-	
-	public List<Set<Long>> getSubListedSet(List<String> normalizedTagList,
-			List<Set<Long>> questionsSetList, int start, int end) {
-		int firstElementsCount = 0;
-		int parenthesizedCount = 0;
-		for (int i = 0; i < end; i++) {
-			if (normalizedTagList.get(i).equals("?")) {
-				if (i < start) {
-					firstElementsCount++;
-				} else {
-					parenthesizedCount++;
-				}
-			}
-		}
-		
-		return questionsSetList.subList(firstElementsCount, firstElementsCount
-				+ parenthesizedCount);
-	}
-	
-	// pre-condition: tagList does not contain parenthesis and is well-formed
-	public Set<Long> reduceGroup(List<String> tagList, List<Set<Long>> questionsSetList) {
-		while (tagList.contains("*")) {
-			int firstANDIndex = tagList.indexOf("*");
-			
-			int leftOperandIndex = -1;
-			for (int i = 0; i < firstANDIndex; i++) {
-				if (tagList.get(i).equals("?")) {
-					leftOperandIndex++;
-				}
-			}
-
-			// compute the INTERSECTION of the two operands
-			Set<Long> leftOperand = questionsSetList.get(leftOperandIndex);
-			Set<Long> rightOperand = questionsSetList.remove(leftOperandIndex+1);
-			leftOperand.retainAll(rightOperand);
-			
-			// update the tagList
-			tagList.remove(firstANDIndex);
-			tagList.remove(firstANDIndex); // this will remove the right-hand operand
-		}
-		
-
-		while (tagList.contains("+")) {
-			int firstANDIndex = tagList.indexOf("+");
-			
-			int leftOperandIndex = -1;
-			for (int i = 0; i < firstANDIndex; i++) {
-				if (tagList.get(i).equals("?")) {
-					leftOperandIndex++;
-				}
-			}
-
-			// compute the UNION of the two operands
-			Set<Long> leftOperand = questionsSetList.get(leftOperandIndex);
-			Set<Long> rightOperand = questionsSetList.remove(leftOperandIndex+1);
-			leftOperand.addAll(rightOperand);
-			
-			// update the tagList
-			tagList.remove(firstANDIndex);
-			tagList.remove(firstANDIndex); // this will remove the right-hand operand
-		}
-		return questionsSetList.get(0);
 	}
 }
